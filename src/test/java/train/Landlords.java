2ce4ba69521edd3a5e76a119a8bb693572204dc8
yangfan <1416880601@qq.com>
2024-08-14 18:31:33 +0800

    修改

---
+++ src/test/java/train/Landlords.java
@@ -0,0 +1,35 @@
+package train;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Landlords {
+    public static void main(String[] args) {
+        String[] name = {"1","2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+        // 准备四种花色
+        String[] color = {"♥","♦","♣","♠"};
+        ArrayList<String> cards = new ArrayList<>();
+        ArrayList<String> newcards = new ArrayList<>();
+        for (String s : name) {
+            for (int i = 0; i < color.length; i++) {
+                cards.add(s+color[i]);
+            }
+        }
+        cards.add("queen1");
+        cards.add("queen2");
+        System.out.println("========开始洗牌========");
+        newcards = washcard(cards);
+        System.out.println(newcards);
+
+    }
+    public static ArrayList<String> washcard(ArrayList<String> cards){
+        Random random = new Random();
+        for (int i = 0; i < cards.size(); i++) {
+            int exchangenum = random.nextInt(cards.size());
+            String a = cards.get(i);
+            cards.set(i, cards.get(exchangenum));
+            cards.set(exchangenum, a);
+        }
+        return cards;
+    }
+}
---
+++ src/test/java/train/StudentTrain/Student.java
@@ -0,0 +1,46 @@
+package train.StudentTrain;
+
+public class Student {
+    private String name;
+    private int mathscore;
+    private int chinesescore;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMathscore() {
+        return mathscore;
+    }
+
+    public void setMathscore(int mathscore) {
+        this.mathscore = mathscore;
+    }
+
+    public int getChinesescore() {
+        return chinesescore;
+    }
+
+    public void setChinesescore(int chinesescore) {
+        this.chinesescore = chinesescore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", mathscore=" + mathscore +
+                ", chinesescore=" + chinesescore +
+                '}';
+    }
+    public void printgetAverage(){
+        System.out.println(this.name+"的平均成绩为:"+(this.mathscore + this.chinesescore) / 2.0);
+    }
+    public void printgetTotal(){
+        System.out.println(this.name+"的总成绩为:"+(this.mathscore + this.chinesescore));
+    }
+}
