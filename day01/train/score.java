ce75ae7a0945185ed4f18b94984c53c4b1b23e0f
yangfan <1416880601@qq.com>
2024-08-14 14:55:23 +0800

    学生对象练习

---
+++ day01/train/score.java
@@ -0,0 +1,21 @@
+package day01.train;
+
+public class score {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("播妞");
+        student1.setMathscore(100);
+        student1.setChinesescore(100);
+        student1.printgetTotal();
+        student1.printgetAverage();
+
+        Student student2 = new Student();
+        student2.setName("播崽");
+        student2.setMathscore(60);
+        student2.setChinesescore(70);
+        student2.printgetTotal();
+        student2.printgetAverage();
+
+
+    }
+}
