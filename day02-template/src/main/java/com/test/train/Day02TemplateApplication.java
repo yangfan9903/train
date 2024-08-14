3edf7446adb34557aa565f7a3f357282523dd47f
diwenzheng <60613511+diwenzheng@users.noreply.github.com>
2024-08-14 23:58:12 +0800

    day02

---
+++ day02-template/src/main/java/com/test/train/Day02TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.test.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day02TemplateApplication.class, args);
+    }
+
+}
