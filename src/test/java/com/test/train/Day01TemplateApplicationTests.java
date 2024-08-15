9df363488a5f0b26dd9bfdb8715d09373e06803d
yangfan <1416880601@qq.com>
2024-08-15 09:41:04 +0800

    修改

---
+++ src/test/java/com/test/train/Day01TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/test/train/MovieTrain/Movie.java
@@ -0,0 +1,49 @@
+package train.MovieTrain;
+
+public class Movie {
+    private int id;
+    private String name;
+    private String actor;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "电影编号：" + id +
+                ", 电影名称：" + name  +
+                ", 电影主演：" + actor +
+                ", 电影票价：" + price +
+                '.';
+    }
+}
