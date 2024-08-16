35e40fd900cf43771546df6b0f7bb830e00545b7
yangfan <1416880601@qq.com>
2024-08-16 09:03:05 +0800

    day03

---
+++ day03-template/src/main/java/com/example/Day03TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day03TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day03TemplateApplication.class, args);
+    }
+
+}
