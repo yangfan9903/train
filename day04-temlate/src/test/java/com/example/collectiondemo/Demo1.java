bc146e7a7081a96fca25b71f1dcc0238123d1f81
yangfan <1416880601@qq.com>
2024-08-19 14:33:45 +0800

    改log.info日志输出

--- day04-temlate/src/test/java/com/example/collectiondemo/Demo1.java
+++ day04-temlate/src/test/java/com/example/collectiondemo/Demo1.java
@@ -1,9 +1,11 @@
 package com.example.collectiondemo;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+@Slf4j
 public class Demo1 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
@@ -14,10 +16,13 @@ public class Demo1 {
 //        list.add("青花瓷");
         // 批量添加数据
         Collections.addAll(list,"hello","world","java","人头马","青花瓷");
-        System.out.println(list);
+        String list2 = list.toString();
+        log.info(list2);
+        log.info("=========================");
 
         Collections.shuffle(list);
-        System.out.println(list);
+        log.info(list.toString());
+        log.info("=========================");
 
         String a = "dasdas";
         String b = "weqdas";
@@ -25,16 +30,19 @@ public class Demo1 {
 
 //        Collections.sort(list,(o1, o2) -> compareTo(o1,o2));
         Collections.sort(list);
-        System.out.println(list);
+        log.info(list.toString());
+        log.info("=========================");
 
         List<Teacher> teacherList = new ArrayList<>();
         teacherList.add(new Teacher("张三", 20, 10000));
         teacherList.add(new Teacher("赵六", 50, 40000));
         teacherList.add(new Teacher("李四", 30, 20000));
         teacherList.add(new Teacher("王五", 40, 30000));
-        System.out.println(teacherList);
+        log.info(teacherList.toString());
+        log.info("=========================");
         Collections.sort(teacherList, (o1, o2) -> o1.getAge() - o2.getAge());
-        System.out.println(teacherList);
+        log.info(teacherList.toString());
+        log.info("=========================");
 
 
     }
