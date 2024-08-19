bce743496e6833002546d1a370d6d7ab1b7be676
yangfan <1416880601@qq.com>
2024-08-19 14:22:43 +0800

    改lombok注解

--- day04-temlate/src/test/java/com/example/collectiondemo/Teacher.java
+++ day04-temlate/src/test/java/com/example/collectiondemo/Teacher.java
@@ -1,46 +1,14 @@
 package com.example.collectiondemo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Teacher {
     private String name;
     private int age;
     private double salary;
-    public Teacher(String name, int age, double salary)
-    {
-        this.name = name;
-        this.age = age;
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", salary=" + salary +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
