912f21b0faae65e982b18bba3b7ff52a72b04756
yangfan <1416880601@qq.com>
2024-08-19 10:05:54 +0800

    Collections API应用练习

---
+++ day04-temlate/src/main/java/com/example/Day04TemlateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day04TemlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day04TemlateApplication.class, args);
+    }
+
+}
