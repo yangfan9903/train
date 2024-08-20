7f12477c37a9ea5ee022923be4afed3e5421011c
yangfan <1416880601@qq.com>
2024-08-20 18:28:47 +0800

    同步锁解决多线程问题

---
+++ day05-template/src/test/java/com/example/demo2/BankUser.java
@@ -0,0 +1,30 @@
+package com.example.demo2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankUser {
+    private String username;
+    private double money;
+    public void saveMoney(double money)
+    {
+        this.money += money;
+    }
+    public void drawMoney(double money)
+    {
+        String name = Thread.currentThread().getName();
+        synchronized (this) {
+            if (this.money>=money){
+                System.out.println(name+"取走了"+money);
+                this.money -= money;
+                System.out.println("该账户剩余"+this.money);
+            }else{
+                System.out.println(name+"取款失败");
+            }
+        }
+    }
+}
---
+++ day05-template/src/test/java/com/example/demo2/Test1.java
@@ -0,0 +1,27 @@
+package com.example.demo2;
+
+public class Test1 {
+    public static void main(String[] args) {
+        BankUser bankUser = new BankUser("123",5000);
+        Thread t1 = new Thread2("小明", bankUser);
+        Thread t2 = new Thread2("小红", bankUser);
+
+            t1.start();
+            t2.start();
+
+    }
+}
+
+class Thread2 extends Thread {
+    private BankUser bankUser;
+    public Thread2(String name, BankUser bankUser) {
+        super(name);
+        this.bankUser = bankUser;
+    }
+    public void run() {
+        bankUser.drawMoney(5000);
+
+    }
+
+
+}
