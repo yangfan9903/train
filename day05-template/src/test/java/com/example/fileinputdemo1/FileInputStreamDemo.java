861b8006e84269d819c21584805b5054d46032e6
yangfan <1416880601@qq.com>
2024-08-20 11:17:05 +0800

    文件字节输入流

---
+++ day05-template/src/test/java/com/example/fileinputdemo1/FileInputStreamDemo.java
@@ -0,0 +1,15 @@
+package com.example.fileinputdemo1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class FileInputStreamDemo {
+    public static void main(String[] args) throws Exception {
+        InputStream is = new FileInputStream("day05-template\\src\\test\\java\\com\\example\\a.txt");
+        int b;
+        while ((b = is.read()) != -1) {
+            System.out.print((char) b);
+        }
+    }
+}
---
+++ day05-template/src/test/java/com/example/fileinputdemo1/FileInputStreamDemo2.java
@@ -0,0 +1,17 @@
+package com.example.fileinputdemo1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class FileInputStreamDemo2 {
+    public static void main(String[] args) throws Exception {
+        InputStream is = new FileInputStream("day05-template\\src\\test\\java\\com\\example\\b.txt");
+        byte[] buffer = new byte[3];
+        int len;
+        while ((len = is.read(buffer)) != -1) {
+            String str = new String(buffer, 0, len);
+            System.out.print(str);
+        }
+    }
+}
