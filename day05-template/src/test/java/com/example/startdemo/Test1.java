4c124b334748abacb45af1a5cd67f0f8886224f0
yangfan <1416880601@qq.com>
2024-08-20 16:21:15 +0800

    继承Tread类实现多线程

---
+++ day05-template/src/test/java/com/example/startdemo/Test1.java
@@ -0,0 +1,20 @@
+package com.example.startdemo;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Thread t1 = new MyThread();
+        t1.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+"-->"+i);
+
+        }
+    }
+}
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+"-->"+i);
+        }
+    }
+}
