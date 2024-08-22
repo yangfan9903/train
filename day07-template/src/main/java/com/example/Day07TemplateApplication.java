362822e5ef6b4e58361e77420ca1833845ab3fb2
yangfan <1416880601@qq.com>
2024-08-22 14:32:50 +0800

    新的一天

---
+++ day07-template/src/main/java/com/example/Day07TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day07TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day07TemplateApplication.class, args);
+    }
+
+}
---
+++ day07-template/src/test/java/com/example/Day07TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day07TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
