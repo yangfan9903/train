347eaaf875bb82c129ceb1736777998c3d688c3e
yangfan <1416880601@qq.com>
2024-08-26 09:29:29 +0800

    新的一天

---
+++ day10-template/src/main/java/com/example/Day10TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day10TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day10TemplateApplication.class, args);
+    }
+
+}
