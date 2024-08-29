247f1c0d46ecc361b78ed4154a78d72dd1c17c91
yangfan <1416880601@qq.com>
2024-08-29 10:38:14 +0800

    新的一天

---
+++ day12-template/src/main/java/com/example/Day12TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day12TemplateApplication.class, args);
+    }
+
+}
