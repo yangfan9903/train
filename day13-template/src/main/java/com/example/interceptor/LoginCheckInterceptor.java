0e3c487621dab5254247207196486a251617901e
yangfan <1416880601@qq.com>
2024-09-02 19:59:05 +0800

    添加AOP对功能进行计时

--- day13-template/src/main/java/com/example/interceptor/LoginCheckInterceptor.java
+++ day13-template/src/main/java/com/example/interceptor/LoginCheckInterceptor.java
@@ -57,12 +57,12 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 //        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-        System.out.println("2============================");
+//        System.out.println("2============================");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 //        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-        System.out.println("3============================");
+//        System.out.println("3============================");
     }
 }
